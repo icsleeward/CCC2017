ff66bcfe0aaf93079ff112033d6a8df4f8c2aa99
Joseph Lorenz <Joseph Lorenz>
2017-03-25 21:29:34 -1000

    added jquery

---
+++ functions.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#hack").click(function(){
+        $("#hack-ans").toggle();
+    });
+    $("#import").click(function(){
+        $("#import-ans").toggle();
+    });
+    $("#challenge").click(function(){
+        $("#challenge-ans").toggle();
+    });
+    $("#accomplish").click(function(){
+        $("#accomplish-ans").toggle();
+    });
+    $("#different").click(function(){
+        $("#different-ans").toggle();
+    });
+});
