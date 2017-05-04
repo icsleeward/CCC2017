90cf179fac6e66b49aa1b365b258d056993d0e6f
Dustin C <pixel.galaxies@gmail.com>
2017-05-03 14:10:17 -1000

    reset phase 2

---
+++ functions.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#hack").click(function(){
+        $("#hack-ans").toggle();
+    });
+    $("#import").click(function(){
+        $("#import-ans").toggle();
+    });
+    $("#challenge").click(function(){
+        $("#challenge-ans").toggle();
+    });
+    $("#accomplish").click(function(){
+        $("#accomplish-ans").toggle();
+    });
+    $("#different").click(function(){
+        $("#different-ans").toggle();
+    });
+    $("#participate").click(function(){
+        $("#participate-ans").toggle();
+        $("#participate-ans-2").toggle();
+        $("#participate-ans-3").toggle();
+    });
+});
